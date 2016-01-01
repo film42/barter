472a63fa9cc6c468b8a841f0e23f01e42e282de8
Garrett Thornburg <garrett.thornburg@moneydesktop.com>
2016-01-01 13:03:00 -0700

    Change formatting on spec printer

--- test/test_helper.hpp
+++ test/test_helper.hpp
@@ -26,7 +26,7 @@ public:
         std::cout << "\033[1;32mpassed\033[0m." << std::endl;
       } else {
         std::cout << "\033[1;31mfailed\033[0m." << std::endl;
-        std::cout << "  Reason: " << output << std::endl;
+        std::cout << "\033[1;31m  Because " << output << "\033[0m" << std::endl;
       }
     }
   }
